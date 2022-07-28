afe644d8eecd98f51ba56d07468a42be6437f49f
shubham-1903 <shubhamkr7677@gmail.com>
2022-07-29 00:38:38 +0530

    fixed navbar icon and toogle mode position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
+import "../App.css";
 const Navbar = () => {
+	const [isOn, setIsOn] = useState(false);
+	const toggleSwitch = () => setIsOn(!isOn);
+	console.log("👻👻: ", isOn);
 	return (
 		<>
-			<div className='flex flex-row justify-start p-3 bg-pgrey'>
-				<a href='https://www.instagram.com/shubham.gupta.1903/' target='_blank'>
-					<img
-						className='h-10 w-10 rounded-3xl'
-						src='../../images/logo.svg'
-						alt='logo'
-					/>
-				</a>
+			{/*  */}
+			<div className='grid grid-cols-2 gap-4  p-3 bg-pgrey justify-evenly'>
+				<div className='  w-20'>
+					<a
+						href='https://www.instagram.com/shubham.gupta.1903/'
+						target='_blank'
+					>
+						<img
+							className='h-10 w-10 rounded-3xl'
+							src='../../images/logo.svg'
+							alt='logo'
+						/>
+					</a>
+				</div>
+				<div className='flex justify-end'>
+					<div
+						className='switch '
+						data-isOn={isOn}
+						onClick={toggleSwitch}
+					>
+						<motion.div className='handle' layout transition={spring} />
+					</div>
+				</div>
 			</div>
 		</>
 	);
 };
-
+const spring = {
+	type: "spring",
+	stiffness: 700,
+	damping: 30,
+};
 export default Navbar;

df8663b11ea4335ee457c82dc5d7e8bde4bd13eb
shubham-1903 <shubhamkr7677@gmail.com>
2022-08-01 17:17:10 +0530

    change image source

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,8 @@ const Header = ({ name }) => {
 				<nav className='flex flex-col justify-center items-center'>
 					<img
 						className='h-20 w-20 rounded-full'
-						src='../../images/sk.jpg'
+						// src='../../images/sk.jpg'
+						src="https://lh3.googleusercontent.com/pw/AL9nZEXwgnIsHzUyw7gdCu5x7f2LHqx-398oKlG_6QPJb9KpV_Q5Rb9Hop5jlJEhUnPpZjxmfW1s6tYppz2nce7Sa4nu_gN8IhGpidsrHXasr8hnAryAoCPkiNDDNFFNoliZRCuVRqaxahsBexXsqlBvb_lP2g=s970-no?authuser=0"
 						alt='sk'
 						// style={{ borderRadius: "20% 80% 20% 80% / 80% 20% 80% 20% "  }}
 					/>
